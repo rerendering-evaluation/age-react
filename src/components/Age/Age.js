fe09b4e6e4773b0c2f8c3d5e0a1d6c9bd4a46c27
Satyajit Gokhale <twisterintwist@gmail.com>
2024-06-07 13:24:13 -0400

    after transformation

--- src/components/Age/Age.js
+++ src/components/Age/Age.js
@@ -1,95 +1,88 @@
-import React, {useEffect, useState} from 'react';
-
+import { memo } from "react";
+import React, { useEffect, useState } from 'react';
 import './AgeStyle.css';
 import Form from "../Form/Form";
-
-
-const Age = () => {
-    const [age, setAge] = useState({years: 0, months: 0, days: 0, hours: 0, minutes: 0, seconds: 0});
-    const [agePerson, setAgePerson] = useState();
-
-    const [years, setYears] = useState(false);
-    const [months, setMonths] = useState(false);
-    const [days, setDays] = useState(false);
-    const [hours, setHours] = useState(false);
-    const [minutes, setMinutes] = useState(false);
-    const [seconds, setSeconds] = useState(false);
-
-    useEffect(() => {
-        const interval = setInterval(() => {
-            let birthday = new Date(agePerson);
-            let now = new Date();
-            let diff = Math.abs(now.getTime() - birthday.getTime());
-
-            const years = Math.floor(diff / (1000 * 60 * 60 * 24 * 365.25));
-            diff -= years * (1000 * 60 * 60 * 24 * 365.25);
-
-            const months = Math.floor(diff / (1000 * 60 * 60 * 24 * 30.44));
-            diff -= months * (1000 * 60 * 60 * 24 * 30.44);
-
-            const days = Math.floor(diff / (1000 * 60 * 60 * 24));
-            diff -= days * (1000 * 60 * 60 * 24);
-
-            const hours = Math.floor(diff / (1000 * 60 * 60));
-            diff -= hours * (1000 * 60 * 60);
-
-            const minutes = Math.floor(diff / (1000 * 60));
-            diff -= minutes * (1000 * 60);
-
-            const seconds = Math.floor(diff / 1000);
-
-            setAge({years, months, days, hours, minutes, seconds});
-        }, 1000);
-
-        return () => clearInterval(interval);
-    }, [agePerson]);
-
-    useEffect(() => {
-        const timerYears = setTimeout(() => {
-            setYears(true);
-        }, 1000);
-        return () => clearTimeout(timerYears);
-    }, []);
-
-    useEffect(() => {
-        const timerMonths = setTimeout(() => {
-            setMonths(true);
-        }, 2000);
-        return () => clearTimeout(timerMonths);
-    }, []);
-
-    useEffect(() => {
-        const timerDay = setTimeout(() => {
-            setDays(true);
-        }, 3000);
-        return () => clearTimeout(timerDay);
-    }, []);
-
-    useEffect(() => {
-        const timerHours = setTimeout(() => {
-            setHours(true);
-        }, 4000);
-        return () => clearTimeout(timerHours);
-    }, []);
-
-    useEffect(() => {
-        const timerMinutes = setTimeout(() => {
-            setMinutes(true);
-        }, 5000);
-        return () => clearTimeout(timerMinutes);
-    }, []);
-
-    useEffect(() => {
-        const timerSeconds = setTimeout(() => {
-            setSeconds(true);
-        }, 6000);
-        return () => clearTimeout(timerSeconds);
-    }, []);
-
-
-    return (
-        <div className={'age'}>
-            <Form setAgePerson={setAgePerson}/>
+const Age = memo(() => {
+  const [age, setAge] = useState({
+    years: 0,
+    months: 0,
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0
+  });
+  const [agePerson, setAgePerson] = useState();
+  const [years, setYears] = useState(false);
+  const [months, setMonths] = useState(false);
+  const [days, setDays] = useState(false);
+  const [hours, setHours] = useState(false);
+  const [minutes, setMinutes] = useState(false);
+  const [seconds, setSeconds] = useState(false);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      let birthday = new Date(agePerson);
+      let now = new Date();
+      let diff = Math.abs(now.getTime() - birthday.getTime());
+      const years = Math.floor(diff / (1000 * 60 * 60 * 24 * 365.25));
+      diff -= years * (1000 * 60 * 60 * 24 * 365.25);
+      const months = Math.floor(diff / (1000 * 60 * 60 * 24 * 30.44));
+      diff -= months * (1000 * 60 * 60 * 24 * 30.44);
+      const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+      diff -= days * (1000 * 60 * 60 * 24);
+      const hours = Math.floor(diff / (1000 * 60 * 60));
+      diff -= hours * (1000 * 60 * 60);
+      const minutes = Math.floor(diff / (1000 * 60));
+      diff -= minutes * (1000 * 60);
+      const seconds = Math.floor(diff / 1000);
+      setAge({
+        years,
+        months,
+        days,
+        hours,
+        minutes,
+        seconds
+      });
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [agePerson]);
+  useEffect(() => {
+    const timerYears = setTimeout(() => {
+      setYears(true);
+    }, 1000);
+    return () => clearTimeout(timerYears);
+  }, []);
+  useEffect(() => {
+    const timerMonths = setTimeout(() => {
+      setMonths(true);
+    }, 2000);
+    return () => clearTimeout(timerMonths);
+  }, []);
+  useEffect(() => {
+    const timerDay = setTimeout(() => {
+      setDays(true);
+    }, 3000);
+    return () => clearTimeout(timerDay);
+  }, []);
+  useEffect(() => {
+    const timerHours = setTimeout(() => {
+      setHours(true);
+    }, 4000);
+    return () => clearTimeout(timerHours);
+  }, []);
+  useEffect(() => {
+    const timerMinutes = setTimeout(() => {
+      setMinutes(true);
+    }, 5000);
+    return () => clearTimeout(timerMinutes);
+  }, []);
+  useEffect(() => {
+    const timerSeconds = setTimeout(() => {
+      setSeconds(true);
+    }, 6000);
+    return () => clearTimeout(timerSeconds);
+  }, []);
+  return <div className={'age'}>
+            <Form setAgePerson={setAgePerson} />
 
             <div className={'textBlock'}>
                 <div className={`textAge ${years ? "showAge" : ""}`}>
@@ -122,8 +115,6 @@ const Age = () => {
                     <p className={'infoAge'}>{agePerson ? age.seconds : 0}</p>
                 </div>
             </div>
-        </div>
-    );
-};
-
+        </div>;
+});
 export default Age;
